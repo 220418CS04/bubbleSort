e8e94a44302fb054bfa5b8f4e7d2f537f5695e0e
220418CS04 <113422113+220418CS04@users.noreply.github.com>
2023-04-25 15:21:07 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
-//
-//  main.cpp
-//  234
-//
-//  Created by Azimjon Axmedov on 25/04/23.
-//
-
 #include <iostream>
 using namespace std;
 void bubbleSort(int arr[], int s){
     for(int i = 0; i<s-1; i++){
-        for(int j = 0; j<s-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+        bool flag = false;
+        for(int j = 0; j<s-i-1;j++){
+            if(arr[j]>arr[j+1]){swap(arr[j],arr[j+1]);flag = true;}
+        }
+        if(!flag){break;}}
+}
+
+void selectionSort (int arr[], int s){
+    int currentStart =0;
+    for(int i = 0; i<s-1;i++){
+        currentStart = i;
+        for(int j = currentStart; j<s;j++){
+            if(arr[j]<arr[currentStart]){
+                currentStart=j;
             }
+        }
+        if(currentStart!=i){
+            swap(arr[currentStart], arr[i]);
+        }
+    }
+}
+
+void insertionSort(int arr[], int s ){
+    for(int i = 0; i<s-1; i++){
+        for(int j = 0; j<s;j++){
         }
     }
 }
 int main(int argc, const char * argv[]) {
     int arr[5]={1,3,2,10,5};
-    bubbleSort(arr, 5);
+    selectionSort(arr, 5);
     for(auto e:arr){
         cout<<e<<endl;
     }
